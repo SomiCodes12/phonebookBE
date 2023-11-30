c86d9d1c7fc7ba385ce6cfe2e6c9a9bc10f9c52d
somicodes12 <somtochukwue98@gmail.com>
2023-11-30 16:09:30 +0100

    update

--- dist/mainApp.js
+++ dist/mainApp.js
@@ -11,5 +11,18 @@ const appConfig = (app) => {
     app.use((0, cors_1.default)());
     app.use(express_1.default.json());
     app.use("/api/v1", phoneBookRouter_1.default);
+    app.get("/", (req, res) => {
+        try {
+            return res.status(200).json({
+                message: "Welcome to Our PhoneBook App 😊😊"
+            });
+        }
+        catch (error) {
+            return res.status(400).json({
+                message: "An Error Occured",
+                reason: error.message
+            });
+        }
+    });
 };
 exports.appConfig = appConfig;
--- mainApp.ts
+++ mainApp.ts
@@ -1,5 +1,5 @@
 import cors from "cors"
-import express, { Application } from "express"
+import express, { Application, Request, Response } from "express"
 import contact from "./Routers/phoneBookRouter"
 
 export const appConfig = ( app : Application ) => {
@@ -7,4 +7,17 @@ export const appConfig = ( app : Application ) => {
     app.use(express.json())
 
     app.use("/api/v1" , contact)
+
+    app.get("/" , (req : Request , res : Response) => {
+try {
+    return res.status(200).json({
+        message : "Welcome to Our PhoneBook App 😊😊"
+    })
+} catch (error : any) {
+    return res.status(400).json({
+        message : "An Error Occured",
+        reason : error.message
+    })
+}
+    })
 }
